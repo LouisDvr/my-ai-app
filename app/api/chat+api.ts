c114dda76005d36656094d89b85075771618f594
louisdev <louisdev@bam.tech>
2025-06-06 11:09:47 +0200

    feat(app): setup a chat api route

---
+++ app/api/chat+api.ts
@@ -0,0 +1,23 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  compatibility: 'strict',
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages,
+  });
+
+  return result.toDataStreamResponse({
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Encoding': 'none',
+    },
+  });
+}
